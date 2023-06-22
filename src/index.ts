c7465df44b5d202bacdda486177b2f1bc74b28ea
Nattapong <banker.z412@gmail.com>
2023-06-22 19:13:12 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { NextFunction, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client"; // here
+
+const app = express();
+const prisma = new PrismaClient(); // here
+
+app.get("/", async (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello World!");
+});
+
+// here
+app.get("/user", async (req: Request, res: Response, next: NextFunction) => {
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers);
+  res.json({ user: allUsers });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
